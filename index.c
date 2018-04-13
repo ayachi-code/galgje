a078dfc91488add81ed745fb2da0a5b1aa2fec27
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-04-13 09:58:03 +0200

    Geheimen woord gemaakt opgeslagen in een char

--- index.c
+++ index.c
@@ -5,6 +5,8 @@
 
 //main functie
 int main(int argc, char **argv){
+    //Geheimen woord gemaakt
+    char geheimen_woord[] = "mensenhoi";
     //input_van_gebruiker met buffer van 5000
     char input_van_gebruiker[5000];
     return 0;
