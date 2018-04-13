cd31599986eeb06472e80a7cf73e6a8cbfd08d4d
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-04-13 09:59:12 +0200

    Begroeting gemaakt en word geprint bij begin

--- index.c
+++ index.c
@@ -5,9 +5,11 @@
 
 //main functie
 int main(int argc, char **argv){
-    //Geheimen woord gemaakt
+    //Geheimen woord gemaakt opgeslagen in een char 
     char geheimen_woord[] = "mensenhoi";
     //input_van_gebruiker met buffer van 5000
     char input_van_gebruiker[5000];
+    //Begroeting gemaakt
+    printf("Welkom bij galgje......... ALPHA versie \n");
     return 0;
 }
