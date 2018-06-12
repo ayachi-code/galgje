27920055155b650b007e09fc80e737091f582003
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-06-12 09:45:36 +0200

    legte van de input van de gebruiker

--- index.c
+++ index.c
@@ -11,17 +11,21 @@ int main(int argc, char **argv){
     //Weet als het in loop zit :)
     int af = 0;
     //Geheimen woord gemaakt opgeslagen in een char 
-    char geheimen_woord[] = "mensen";
+    char geheimen_woord[] = "issam";
     //input_van_gebruiker met buffer van 5000
     char input_van_gebruiker[5000];
     //Begroeting gemaakt
     printf("Welkom bij galgje......... ALPHA versie \n");
+    //Zegt dat je een letter moet typen
+    printf("Type een letter: ");
     //loopt als af 0 is
     while (af == 0) {
         //Leest input van gebruiker en input word daarna opgeslagen in variablen input van gebruiker
         scanf("%s",input_van_gebruiker);
         //Lengte van geheimen woord opgeslagen in int
         int lengte_van_strint = strlen(geheimen_woord);
+        //legte van de input van de gebruiker
+        int lengte_van_input = strlen(input_van_gebruiker);
         //Als woord in een keer geraden is
         if (!strcmp(input_van_gebruiker,geheimen_woord)) {
             printf("Goed zo je hebt het woord geraden... \n");
@@ -30,15 +34,15 @@ int main(int argc, char **argv){
             printf("Jammer : ( probeer nog eens \n ");
             kansen -= 1;
             printf("%d",kansen);
-        }
+        }       
         if (kansen <= 0) {
-            printf("je bent af \n");
+            printf("sorry je bent af \n");
             break;
-        }
+        }      
         //Print aantal streepjes van woord
         for (int i = 0;i < lengte_van_strint;i++) {
             printf("- ");
-        }
+        }   
     }
     return 0;
 }
